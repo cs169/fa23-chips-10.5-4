b2941924edec2a3e46e80dcb0a37a2d39dc9f8cd
Felipe Magdaleno <felipemag07@berkeley.edu>
2023-11-21 07:06:24 +0000

    Test the GitHub Action 5

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -16,8 +16,12 @@ class Representative < ApplicationRecord
           ocdid_temp = office.division_id
         end
       end
-      
+
       rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
           title: title_temp })
       reps.push(rep)
     end
+    
+    reps
+  end
+end
